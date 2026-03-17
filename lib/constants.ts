2a130eaf7f3899ea2b0cffea948e742df405df6b
Dinesh Mistry <dmistry@yourhostdirect.com>
2026-03-17 09:06:05 -0400

    fix: set waitlist Apps Script URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
 export const APP_URL = "https://dev.shipfeat.ai";
-export const WAITLIST_URL = "PLACEHOLDER_APPS_SCRIPT_URL";
+export const WAITLIST_URL = "https://script.google.com/macros/s/AKfycbxpIfN-KJu3EEm6pIr4zal6jaRFn0womT8j4--DQtvaJMi6YwPmYfVWuDf41csjkJ1Z5A/exec";
